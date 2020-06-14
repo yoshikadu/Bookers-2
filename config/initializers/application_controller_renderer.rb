40af60f3504ffbecae61c8cc968f1a6ce9d73f62
yoshikadu <yoshikadu5545@gmail.com>
2020-06-14 14:20:43 +0900

    refile secret key設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c146d1f56d033b30ea293eb6326e656da5c81f119b95db9c89928b3b2b1842d73d35358fc051807dbdb1a83e7b9f20ee3a709332ecfbf9dfaa96463b6325f741'
